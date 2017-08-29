LineageOS/android_device_sony_msm8974-common

msm8974-common: inherit proprietary blobs
Change-Id: Ifd76c260ab3390f0ab5e2f29748b30b6262ceb64

msm8974.mk
@@ -125,3 +125,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
+
+# Include non-opensource parts
+$(call inherit-product, vendor/sony/shinano-common/msm8974-common-vendor.mk)

AICP/device_sony_msm8974-common

msm8974.mk
@@ -157,3 +157,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
+
+# Include non-opensource parts
+$(call inherit-product, vendor/sony/msm8974-common/msm8974-common-vendor.mk)