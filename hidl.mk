eb404903c7312cd1d7635ffab51b70137f11f1bd
nailyk-fr <jenkins@nailyk.fr>
2017-10-16 20:26:39 +0200

    msm8974-common: sepolicy: Relabel /idd
     * With the SODP split, some labelling gones.
       Relabel /idd correctly
    
    Change-Id: I43e5f0d3f416b4a4e358dac3a30c175e6c5ac0fb

--- hidl.mk
+++ hidl.mk
@@ -11,6 +11,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl \
 	android.hardware.camera.provider@2.4 \
+	vendor.qti.hardware.camera.device@1.0_hal \
 	android.hardware.camera.device@1.0
 
 # DRM
