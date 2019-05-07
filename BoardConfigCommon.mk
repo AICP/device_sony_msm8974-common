9babc1c57937b7c3ae1175f72a2d927b82aeec9d
arian <arian.kulmer@web.de>
2019-05-07 18:14:39 +0200

    msm8974-common: Pass Sony Open Devices build barrier
    Change-Id: Ib1368bb80bd16f8c1bc2845e5df13ad7c91bb9a9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,6 +149,9 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
 TARGET_LD_SHIM_LIBS += \
     /system/vendor/lib/libril-qc-qmi-1.so|libaudioclient_shim.so
 
+# SODP build barrier
+PRODUCT_PLATFORM_SOD := true
+
 # Treble
 DEVICE_MANIFEST_FILE := device/sony/msm8974-common/treble-manifest.xml
 ifneq ($(BOARD_HAVE_RADIO),false)
