7457d60b9563cbc8e42ca2252b62952c35896b3a
SpiritCroc <dev@spiritcroc.de>
2019-09-13 12:50:26 +0200

    [TMP] msm8974: Disable sepolicy
    Change-Id: I8513946de61765058e05472dadc77bd7cfa3c380

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,10 +132,10 @@ TARGET_USES_INTERACTION_BOOST := true
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
+#include device/qcom/sepolicy-legacy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    device/sony/msm8974-common/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    device/sony/msm8974-common/sepolicy
 
 # Treble
 DEVICE_MANIFEST_FILE := device/sony/msm8974-common/treble-manifest.xml
