1bde116aca963f84dd00cb0723365915d47235c7
koron393 <koron393@gmail.com>
2019-09-15 23:12:55 +0900

    [TMP] msm8974-common: Revert HIDL power service to generic 1.0
    Change-Id: Ifd48ace913c0f014688eeea7775f622c2f91e570

--- hidl.mk
+++ hidl.mk
@@ -68,7 +68,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Rendering
 PRODUCT_PACKAGES += \
